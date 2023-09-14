bbf487f573589f6878e8e0f23f59e2a50379a944
Petro Arva <petroarva33@gmail.com>
2023-09-14 14:58:55 +0200

    download image

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,17 +47,35 @@ const Home = () => {
     <div>
       <Navbar />
       <main className="flex flex-col items-center justify-center">
-        <button
-          onClick={showImage ? takePhotoAgain : onTakePhoto}
-          className="my-5 relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-        >
-          <span className="flex justify-center items-center relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            <p className="from-slate-200 mr-5">
-              {showImage ? "Take again" : "Capture your photo"}
-            </p>
-            <Image src={CameraIcon} width={30} height={30} alt="camera icon" />
-          </span>
-        </button>
+        <div className="flex flex-col items-center justify-center">
+          <button
+            onClick={showImage ? takePhotoAgain : onTakePhoto}
+            className="my-5 relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
+          >
+            <span className="flex justify-center items-center relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              <p className="from-slate-200 mr-5">
+                {showImage ? "Take again" : "Capture your photo"}
+              </p>
+              <Image
+                src={CameraIcon}
+                width={30}
+                height={30}
+                alt="camera icon"
+              />
+            </span>
+          </button>
+
+          {showImage && (
+            <a href={image} download={image}>
+              <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Download image
+                </span>
+              </button>
+            </a>
+          )}
+        </div>
+
         {showImage ? (
           <Image
             src={image!}
