f91d050a41741d6a3af0f28da22ad2daedfa0dd0
Petro Arva <petroarva33@gmail.com>
2023-09-14 16:04:30 +0200

    add qrcode page route

---
+++ pages/qrcodePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+const qrcodePage = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default qrcodePage;
